03180e89f125c4ee45eba9f959afbb17f8f85c84
Connor <ccarmic2@uccs.edu>
2025-03-20 14:44:02 -0600

    Got tls handshake working on client. using 443 to test.

--- client.c
+++ client.c
@@ -10,8 +10,8 @@
 #include <unistd.h>
 #include <openssl/ssl.h>
 
-#define SERVER_PORT 80                                                  //client and server must agree on port number
-//#define SERVER_PORT 443                                                  //client and server must agree on port number
+//#define SERVER_PORT 80                                                //client and server must agree on port number
+#define SERVER_PORT 443                                                 //client and server must agree on port number
 #define BUF_SIZE 4096                                                   //block transfer size (used for file transfer)
 
 void fatal(const char *string);
@@ -20,16 +20,16 @@ int main(int argc, char **argv){
     int c, s, bytes;
     char buf[BUF_SIZE];                                                 //buffer for incoming file
     struct sockaddr_in channel;                                         //holds IP address
-    char* request = "GET / HTTP/1.1\r\n\r\n";
+    char* request = "GET /\r\n\r\n";
 
     //create socket
-    s = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);                      //create socket
+    s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);                      //create socket
     if (s < 0) fatal("socket failed");                             
     printf("created socket\n");
 
     //set ip adress and port
     memset(&channel, 0, sizeof(channel));
-    channel.sin_family=AF_INET;
+    channel.sin_family = AF_INET;
     channel.sin_port = htons(SERVER_PORT);
     if (argc != 2) fatal("Usage: client server-ip");
     //convert ip address to machine readable bytes    
@@ -38,21 +38,26 @@ int main(int argc, char **argv){
 
     c = connect(s, (struct sockaddr *) &channel, sizeof(channel));      //establish tcp connection with server
     if (c < 0) fatal("connect failed");
-    printf("connectd\n");
+    printf("connected\n");
+
+    //establish tls connection
+    SSL_CTX* ctx = SSL_CTX_new(TLS_method());                           //create ctx object that holds config data for TLS
+    SSL* ssl = SSL_new(ctx);                                            //SSL structure needed for a connection
+    SSL_set_fd(ssl, s);                                                 //Pass the socket(file descriptor) to SSL structure
+    if (SSL_connect(ssl) <= 0) fatal("tls handshake not successful");   //initiates TLS handshake
+    printf("Handshake was successful\n");
 
     //Connection is now established; Send request
-    write(s, request, strlen(request));                                 //write to the socket the file name
+    SSL_write(ssl, request, strlen(request));                           //write to the socket the file name
     printf("sending request \n");
 
 
     //Go get the file and write it to stdout
     printf("request reciving \n");
-    while (1) {
-        memset(buf, 0, BUF_SIZE);                                       //zero out recive buffer each loop
-        bytes = read(s, buf, BUF_SIZE);                                 //read from socket
-        if (bytes <= 0) exit(0);                                        //check for end of file
-        printf("%s\n", buf);                                            //write to stdout
-    }
+    memset(buf, 0, BUF_SIZE);                                           //zero out recive buffer each loop
+    bytes = SSL_read(ssl, buf, (BUF_SIZE-1));                           //read from socket
+    //if (bytes <= 0) exit(0);                                            //check for end of file
+    printf("%s\n", buf);                                                //write to stdout
     printf("request recived\n");
 }
 
